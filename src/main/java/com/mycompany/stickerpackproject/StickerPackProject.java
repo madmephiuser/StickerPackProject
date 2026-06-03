4c57b4c4ec589cb6376188a161809b9743a87075
madmephiuser <m.visloushkina@yandex.ru>
2026-06-03 18:10:03 +0300

    Создание проекта и настройка зависимостей
    pom.xml с зависимостями Spring Web, JPA, Security, PostgreSQL, GigaChat SDK

---
+++ src/main/java/com/mycompany/stickerpackproject/StickerPackProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stickerpackproject;
+
+/**
+ *
+ * @author Professional
+ */
+public class StickerPackProject {
+
+    public static void main(String[] args) {
+        
+    }
+}
